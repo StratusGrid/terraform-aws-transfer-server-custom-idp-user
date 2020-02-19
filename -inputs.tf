1efdefdff2afb674c4065cf246f98e3949b6da2b
Ivan Casco <ivan@ivancasco.com>
2020-02-19 01:58:26 +0100

    Initial version

---
+++ -inputs.tf
@@ -0,0 +1,49 @@
+variable "s3_bucket_name" {
+  description = "Transfer Server S3 bucket name"
+  type        = string
+}
+
+variable "transfer_server_id" {
+  description = "Transfer Server ID"
+  type        = string
+}
+
+variable "user_name" {
+  description = "User name for SFTP server"
+  type        = string
+}
+
+variable "user_home" {
+  description = "HOME path for transfer server user"
+  type        = string
+  default     = "/"
+}
+
+variable "ssh_key" {
+  description = "SSH Key for transfer server user"
+  type        = string
+  default     = ""
+}
+
+variable "secrets_prefix" {
+  description = "Prefix used to create AWS Secrets"
+  default     = "SFTP"
+  type        = string
+}
+
+variable "input_tags" {
+  description = "Map of tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "name_prefix" {
+  description = "String to use as prefix on object names"
+  type        = string
+}
+
+variable "name_suffix" {
+  description = "String to append to object names. This is optional, so start with dash if using"
+  type        = string
+  default     = ""
+}
---
+++ main.tf
@@ -0,0 +1,82 @@
+resource "aws_iam_role" "sftp_transfer_server_user" {
+  name = "${var.name_prefix}-sftp-transfer-server-user-${var.user_name}-iam-role${var.name_suffix}"
+
+  assume_role_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+        "Effect": "Allow",
+        "Principal": {
+            "Service": "transfer.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+        }
+    ]
+}
+EOF
+  tags = merge(
+    var.input_tags,
+    {
+      "Name" = "${var.name_prefix}-sftp-transfer-server-user-${var.user_name}-role${var.name_suffix}"
+    },
+  )
+}
+
+resource "aws_iam_role_policy" "sftp_transfer_server_user" {
+  name = "${var.name_prefix}-sftp-transfer-server-user-${var.user_name}-iam-policy${var.name_suffix}"
+  role = aws_iam_role.sftp_transfer_server_user.id
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "AllowListingOfUserFolder",
+            "Action": [
+                "s3:ListBucket",
+                "s3:GetBucketLocation"
+            ],
+            "Effect": "Allow",
+            "Resource": [
+                "arn:aws:s3:::${var.s3_bucket_name}"
+            ]
+        },
+        {
+            "Sid": "HomeDirObjectAccess",
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutObject",
+                "s3:GetObject",
+                "s3:DeleteObjectVersion",
+                "s3:DeleteObject",
+                "s3:GetObjectVersion"
+            ],
+            "Resource": "arn:aws:s3:::${var.s3_bucket_name}${var.user_home}*"
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_secretsmanager_secret" "secret" {
+  name = "${var.secrets_prefix}/${var.name_prefix}-sftp-${var.user_name}${var.name_suffix}"
+  tags = merge(
+    var.input_tags,
+    {
+      "Name" = "${var.name_prefix}-sftp-transfer-server-user-${var.user_name}${var.name_suffix}"
+    },
+  )
+}
+
+resource "aws_secretsmanager_secret_version" "secret" {
+  secret_id     = aws_secretsmanager_secret.secret.id
+  secret_string = <<EOF
+{
+  "Password": "ChangeMe",
+  "PublicKey": "${var.ssh_key}",
+  "Role": "${aws_iam_role.sftp_transfer_server_user.arn}",
+  "HomeDirectory": "/${var.s3_bucket_name}${var.user_home}"
+}
+EOF
+}
