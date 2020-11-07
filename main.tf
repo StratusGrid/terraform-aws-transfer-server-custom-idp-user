a2702f443ca96a45522163f20993afc9c09a76c1
Ivan Casco <ivan.casco@stratusgrid.com>
2020-11-07 01:15:00 +0100

    Add PutObjectAcl permission to support cross-account access

--- main.tf
+++ main.tf
@@ -47,17 +47,19 @@ resource "aws_iam_role_policy" "sftp_transfer_server_user" {
             "Effect": "Allow",
             "Action": [
                 "s3:PutObject",
+                "s3:PutObjectAcl",
                 "s3:GetObject",
                 "s3:DeleteObjectVersion",
                 "s3:DeleteObject",
-                "s3:GetObjectVersion"
-            ],
+                "s3:GetObjectVersion",
+                ],
             "Resource": "arn:aws:s3:::${var.s3_bucket_name}${var.user_home}*"
         }
     ]
 }
 POLICY
 }
+
 
 resource "aws_secretsmanager_secret" "secret" {
   name = "${var.secrets_prefix}/${var.name_prefix}-sftp-${var.user_name}${var.name_suffix}"
